71ab72faebe7963d9d391788127efaec7f457bb0
developerpenis <66734742+developerpenis@users.noreply.github.com>
2024-05-19 23:47:58 -0300

    Add files via upload

---
+++ jscript clase 10/script.js
@@ -0,0 +1,13 @@
+ var nombre = "Laureano" //string
+ var apellido = "Miquelarena" // string
+
+ console.log("Bienvenido " + nombre + " " + apellido)
+
+var numero_1 =72; //numerico
+var numero_2 =91; //numerico
+var suma = numero_1 + numero_2;
+
+console.log("El resultado de la suma es " + suma)
+
+var numero_3 = suma + 83;
+console.log("El resultado de la suma más 83 es " + numero_3)
---
+++ jscript clase 11/script.js
@@ -0,0 +1,62 @@
+var topping = "oreo";
+var topping = "kitkat";
+var topping = "kinder";
+var precio1=10;
+var precio2=15;
+var precio3=25;
+var preciofinal_1= 15 + 10;
+var preciofinal_2= 15 + 15;
+var preciofinal_3= 15 + 25;
+var helado=15;
+
+var topping = prompt("Dime que topping quieres comprar");
+
+if(topping === "oreo"){
+    console.log("El valor de el topping seleccionado es " + precio1 + "  " + "El precio final seria " + preciofinal_1
+)
+}
+    
+if(topping === "kitkat"){
+    console.log("El valor de el topping seleccionado es " + precio2 + "  " + "El precio final seria " + preciofinal_2)
+}
+
+if(topping === "kinder"){
+    console.log("El valor de el topping selecionado es " + precio3 + "  " + "El precio final seria " + preciofinal_3)
+}
+
+var pedido = prompt("Elige un menu")
+
+
+switch(pedido.toLowerCase()) {
+    case "carne":
+        console.log("Si eliges carne, viene incluido con un vino tinto")
+        break;
+    case "pescado":
+        console.log("Si eliges pescado, viene incluido con un vino blanco")
+        break;
+    case "verdura":
+        console.log("Si eliges verdura, viene incluido con un agua")
+}
+
+
+ var array=[1,6,86,594,239]
+
+console.log();
+
+let index = array.length -1;
+for (index ; index >= 0 ; index--) {
+    alert("Tu numero es " + array[index] );
+    console.log(array[index]);
+}
+
+var numero=6;
+while(numero==6){
+    console.log(numero)
+    console.log(numero + " es más chico que 11")
+
+    numero++
+} 
+
+
+
+

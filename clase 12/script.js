ad74586649281b742eaf03e661ddd5e1350f000a
developerpenis <66734742+developerpenis@users.noreply.github.com>
2024-05-26 22:59:48 -0300

    Add files via upload

---
+++ clase 12/script.js
@@ -0,0 +1,11 @@
+let persona= prompt("Ingrese su nombre");
+
+if(persona == "", "1,2,3,4,5,6,7,8,9"){
+    alert("Ingrese un nombre valido por favor")
+} else if(persona === persona){
+    alert("Bienvenido a mi página web " +persona)
+}
+
+
+    
+
